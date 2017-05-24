be19e3de5f5c721683dc5d4bd1145821bca16d93
lokyoung <lokyoung@outlook.com>
2017-05-24 23:07:33 +0800

    change index page schedule

--- js/custom.js
+++ js/custom.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     //sticky navbar
     $(".navbar-sticky").sticky({topSpacing: 0});
     //countDown js
-    $('#countdown').countdown('2017/06/11 09:30:00', function(event) {
+    $('#countdown').countdown('2017/06/10 09:30:00', function(event) {
       var $this = $(this).html(event.strftime(''
         + '<span class="countdown-unit"><span class="countdown-unit__digit">%D</span><br><span>Days</span></span>'
         + '<span class="countdown-unit"><span class="countdown-unit__digit">%H</span><br><span>Hours</span></span>'
